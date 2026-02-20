5bfb0e384d35ce27ca46165f866a4af407f64e4b
palloHiiri <408322@niuitmo.ru>
2026-02-20 15:05:41 +0300

    putchar getchar

--- run.sh
+++ run.sh
@@ -5,11 +5,13 @@ QEMU=qemu-system-riscv32
 
 # Путь к clang и его флагам
 CC=/opt/homebrew/opt/llvm/bin/clang  # Для Ubuntu: используйте CC=clang
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib -fuse-ld=lld"
 
 # Сборка ядра
-$CC $CFLAGS -Wl, -Tkernel.ld -Wl, -Map=kernel.map -o kernel.elf kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 
 # Запуск QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
+
+    
